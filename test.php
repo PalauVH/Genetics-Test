623bbe181e5f16ae0a611274b6cf92905d035100
Palau van Helden <palauvanhelden02@gmail.com>
2022-11-02 20:07:00 +0100

    modified:   test.php

--- test.php
+++ test.php
@@ -1,8 +1,4 @@
 <?php
-echo "Dit is een testbestand";
-echo "<br>Met een edit";
-
-echo "<br>Dit is een test voor een extra tak";
-echo "Dit is een extra tak";
+echo "KIjken hoesnel deze verandering geupload is";
 ?>
 
